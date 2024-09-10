3be7c50dc9799e23dea57b7582e52a35c8886ef2
Gurge44 <gurge44.minecraft@gmail.com>
2024-09-10 14:10:54 +0200

    Remove unused code

--- Foci/Match.cs
+++ Foci/Match.cs
@@ -2,9 +2,9 @@
 
 public class Match(int round, int homeGoals, int enemyGoals, int halftimeHomeGoals, int halftimeEnemyGoals, string homeTeamName, string enemyTeamName)
 {
-    public int Round { get; set; } = round;
-    public Team HomeTeam { get; set; } = new(homeGoals, halftimeHomeGoals, homeTeamName);
-    public Team EnemyTeam { get; set; } = new(enemyGoals, halftimeEnemyGoals, enemyTeamName);
+    public int Round { get; } = round;
+    public Team HomeTeam { get; } = new(homeGoals, halftimeHomeGoals, homeTeamName);
+    public Team EnemyTeam { get; } = new(enemyGoals, halftimeEnemyGoals, enemyTeamName);
 
     public Team GetWinnerTeam()
     {
@@ -13,24 +13,11 @@ public class Match(int round, int homeGoals, int enemyGoals, int halftimeHomeGoa
         return null;
     }
 
-    public Team GetHalfTimeWinnerTeam()
-    {
-        if (HomeTeam.HalftimeGoals > EnemyTeam.HalftimeGoals) return HomeTeam;
-        if (HomeTeam.HalftimeGoals < EnemyTeam.HalftimeGoals) return EnemyTeam;
-        return null;
-    }
-
-    public bool IsDraw() => HomeTeam.Goals == EnemyTeam.Goals;
-    public bool IsHalfTimeDraw() => HomeTeam.HalftimeGoals == EnemyTeam.HalftimeGoals;
-
     public bool IsHomeWin() => HomeTeam.Goals > EnemyTeam.Goals;
     public bool IsEnemyWin() => HomeTeam.Goals < EnemyTeam.Goals;
 
     public bool IsHalfTimeHomeWin() => HomeTeam.HalftimeGoals > EnemyTeam.HalftimeGoals;
     public bool IsHalfTimeEnemyWin() => HomeTeam.HalftimeGoals < EnemyTeam.HalftimeGoals;
-
-    public bool IsHomeLeadAtHalfTime() => HomeTeam.HalftimeGoals > EnemyTeam.HalftimeGoals;
-    public bool IsEnemyLeadAtHalfTime() => HomeTeam.HalftimeGoals < EnemyTeam.HalftimeGoals;
 }
 
 public class Team(int goals, int halftimeGoals, string name)
