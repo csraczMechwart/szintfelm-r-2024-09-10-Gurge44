a553206a6b32188cb2abf7638452db21a6d16f64
Gurge44 <gurge44.minecraft@gmail.com>
2024-09-10 13:01:25 +0200

    Setup done

---
+++ Foci/Match.cs
@@ -0,0 +1,12 @@
+﻿namespace Foci;
+
+public class Match(int round, int homeGoals, int enemyGoals, int halftimeHomeGoals, int halftimeEnemyGoals, string homeTeamName, string enemyTeamName)
+{
+    public int Round { get; set; } = round;
+    public int HomeGoals { get; set; } = homeGoals;
+    public int EnemyGoals { get; set; } = enemyGoals;
+    public int HalftimeHomeGoals { get; set; } = halftimeHomeGoals;
+    public int HalftimeEnemyGoals { get; set; } = halftimeEnemyGoals;
+    public string HomeTeamName { get; set; } = homeTeamName;
+    public string EnemyTeamName { get; set; } = enemyTeamName;
+}
---
+++ Foci/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Foci;
+
+class Program
+{
+    private static List<Match> Matches = [];
+    private static int TaskNum;
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    static void ReadDataFromFile()
+    {
+        const string path = "meccs.txt";
+        File.ReadAllLines(path)[1..].Do(CreateMatchFromLine);
+    }
+
+    static void CreateMatchFromLine(string line)
+    {
+        var parts = line.Split(' ');
+        var match = new Match(
+            int.Parse(parts[0]),
+            int.Parse(parts[1]),
+            int.Parse(parts[2]),
+            int.Parse(parts[3]),
+            int.Parse(parts[4]),
+            parts[5],
+            parts[6]
+        );
+        Matches.Add(match);
+    }
+
+    static void PrintResult(string result)
+    {
+        TaskNum++;
+        Console.WriteLine($"{TaskNum}. feladat: {result}");
+    }
+    
+    static int RequestNumberInput(string message)
+    {
+        Console.Write(message);
+        var input = Console.ReadLine();
+        while (!int.TryParse(input, out _))
+        {
+            Console.WriteLine("Nem számot adott meg!");
+            Console.Write(message);
+            input = Console.ReadLine();
+        }
+        
+        return int.Parse(input);
+    }
+}
